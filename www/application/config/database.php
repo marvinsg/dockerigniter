ae1886576885ebb4fa80071ca6ef7fb7406cd03f
Marvin Soriano <marvin.soriano@promocionesfarma.com>
2020-03-12 18:14:23 +0100

    Implementing config file and database config file depending the environment

--- www/application/config/database.php
+++ www/application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'          => '',
-	'hostname'     => '127.0.0.1',
-	'username'     => 'admin',
-	'password'     => 'Foo12345',
-	'database'     => 'dockerigniter',
+	'hostname'     => '',
+	'username'     => '',
+	'password'     => '',
+	'database'     => '',
 	'dbdriver'     => 'mysqli',
 	'dbprefix'     => '',
 	'pconnect'     => FALSE,

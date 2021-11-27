4579cfd1981d691f7ea6e59f51e0c96ac9fc3bbf
Marvin Soriano <marvin.soriano.cat@gmail.com>
2021-11-27 01:07:59 +0100

    Testing if PHPStan works

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+use PHPStan\Type\Traits\MaybeCallableTypeTrait;
+
 class HomeController extends MY_Controller
 {
 	public function index()
@@ -16,5 +18,9 @@ class HomeController extends MY_Controller
 	public function foo()
 	{
 		echo "<h2>This is the Foo EndPoint</h2>";
+	}
+
+	public function testPhpStan()  {
+	return true;
 	}
 }

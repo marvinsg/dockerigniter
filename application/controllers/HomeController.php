55322e67db944dc5f97510159ff30a3116814459
Marvin Soriano <marvin.soriano.cat@gmail.com>
2021-11-27 01:09:55 +0100

    Removing test function

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -1,9 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-use PHPStan\Type\Traits\MaybeCallableTypeTrait;
-
-class HomeController extends MY_Controller
+final class HomeController extends MY_Controller
 {
 	public function index()
 	{
@@ -18,9 +16,5 @@ class HomeController extends MY_Controller
 	public function foo()
 	{
 		echo "<h2>This is the Foo EndPoint</h2>";
-	}
-
-	public function testPhpStan()  {
-	return true;
 	}
 }
